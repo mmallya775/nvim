6d1a811030966143d5e72f062c024d63c4a24e4f
Mohit Radhesh Mallya <63004249+mmallya775@users.noreply.github.com>
2025-07-12 22:40:36 +0200

    Bufferline added

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,22 +1,25 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  opts = {},
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false, -- neo-tree will lazily load itself
+	opts = {},
 
-  config = function()
-    require("neo-tree").setup({
-      filesystem = {
-        filtered_items = {
-          visible = true,
-        },
-      },
-    })
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-  end,
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+				},
+			},
+			window = {
+				position = "left", -- always open on the left
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", { noremap = true, silent = true })
+	end,
 }
