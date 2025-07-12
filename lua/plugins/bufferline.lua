34ba95f5a7408596f86652ad5b88e34f7f9b4101
Mohit Radhesh Mallya <63004249+mmallya775@users.noreply.github.com>
2025-07-13 00:04:31 +0200

    Updates

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,36 +1,36 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
 
-	config = function()
-		vim.opt.termguicolors = true
-		require("bufferline").setup({
-			options = {
-				sort_by = "id",
-			},
-			highlights = {
-				buffer_selected = {
-					underline = true,
-					underline_style = "undercurl",
-					italic = false,
-					bold = true,
-					sp = "#ff0000", -- red underline color
-					fg = "#000000", -- black text for contrast on light background
-					bg = "#f0f0f0",
-				},
-			},
-		})
-		-- Set keymaps for cycling buffers
-		vim.keymap.set("n", "<leader>bn", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
-		vim.keymap.set("n", "<leader>bp", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
-		for i = 1, 9 do
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>" .. i,
-				":BufferLineGoToBuffer " .. i .. "<CR>",
-				{ noremap = true, silent = true }
-			)
-		end
-	end,
+  config = function()
+    vim.opt.termguicolors = true
+    require("bufferline").setup({
+      options = {
+        sort_by = "id",
+      },
+      highlights = {
+        buffer_selected = {
+          underline = true,
+          underline_style = "undercurl",
+          italic = false,
+          bold = true,
+          sp = "#ff0000",
+          -- fg = "#000000", -- black text for contrast on light background
+          -- bg = "#f0f0f0",
+        },
+      },
+    })
+    -- Set keymaps for cycling buffers
+    vim.keymap.set("n", "<leader>bn", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>bp", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+    for i = 1, 9 do
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>" .. i,
+        ":BufferLineGoToBuffer " .. i .. "<CR>",
+        { noremap = true, silent = true }
+      )
+    end
+  end,
 }
