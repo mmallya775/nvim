d77e625d119ee9584defc97858d838f980275be5
Mohit Radhesh Mallya <63004249+mmallya775@users.noreply.github.com>
2025-07-12 17:44:20 +0200

    Plugin for easier commenting

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,15 +1,15 @@
 return {
   "rose-pine/neovim",
   name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      styles = {
-        bold = true,
-        italic = false,
-        transparency = false,
-      },
-    })
-
-    --   vim.cmd("colorscheme rose-pine")
-  end,
+  -- config = function()
+  --   require("rose-pine").setup({
+  --     styles = {
+  --       bold = true,
+  --       italic = false,
+  --       transparency = false,
+  --     },
+  --   })
+  --
+  --   vim.cmd("colorscheme rose-pine")
+  -- end,
 }
