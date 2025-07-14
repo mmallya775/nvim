ee5d560c7d080b8b1311ffd6630c14204898e99d
mmallya775 <mmallya775@gmail.com>
2025-07-14 20:55:44 +0200

    a workaround for fuzzy find

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,112 +1,123 @@
 return {
 
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-live-grep-args.nvim",
-		},
-		config = function()
-			local telescope = require("telescope")
-			local builtin = require("telescope.builtin")
-			local lga_actions = require("telescope-live-grep-args.actions")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+      local lga_actions = require("telescope-live-grep-args.actions")
 
-			telescope.setup({
-				extensions = {
-					live_grep_args = {
-						auto_quoting = true,
-						mappings = {
-							i = {
-								["<C-k>"] = lga_actions.quote_prompt(),
-								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-							},
-						},
-					},
-				},
-				defaults = {
-					vimgrep_arguments = {
-						"rg",
-						"--color=never",
-						"--no-heading",
-						"--with-filename",
-						"--line-number",
-						"--column",
-						"--smart-case",
-					},
+      telescope.setup({
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              },
+            },
+          },
+        },
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
 
-					file_ignore_patterns = {
-						"./node_modules/*",
-						"node_modules",
-						"^node_modules/*",
-						"node_modules/*",
-						"target/*",
-						"^target/*",
-						"target",
-					},
-					-- In your defaults table
-					defaults = {},
-				},
-			})
+          file_ignore_patterns = {
+            "./node_modules/*",
+            "node_modules",
+            "^node_modules/*",
+            "node_modules/*",
+            "target/*",
+            "^target/*",
+            "target",
+          },
+          -- In your defaults table
+          defaults = {},
+        },
+      })
 
-			telescope.load_extension("live_grep_args")
+      telescope.load_extension("live_grep_args")
 
-			-- Mappings
-			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set(
-				"n",
-				"<leader>fg",
-				telescope.extensions.live_grep_args.live_grep_args,
-				{ desc = "Live Grep with Args" }
-			)
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP References" })
-			vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Find References" })
-			vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run, { desc = "Run CodeLens" })
-			vim.keymap.set(
-				"n",
-				"<leader>ds",
-				"<cmd>Telescope lsp_document_symbols<CR>",
-				{ noremap = true, silent = true }
-			)
+      -- Mappings
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set(
+        "n",
+        "<leader>fg",
+        telescope.extensions.live_grep_args.live_grep_args,
+        { desc = "Live Grep with Args" }
+      )
 
-			-- Highlight groups can go here or elsewhere, but after termguicolors = true
-		end,
-	},
+      vim.keymap.set(
+        "n",
+        "<leader>ff",
+        ":Telescope resume<CR>",
+        {
+          desc =
+          "Open the previously selected grep window again to account for cases where it didn't jump to correct line",
+        }
+      )
 
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-							defaults = {
-								border = true,
-							},
-						}),
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP References" })
+      vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Find References" })
+      vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run, { desc = "Run CodeLens" })
+      vim.keymap.set(
+        "n",
+        "<leader>ds",
+        "<cmd>Telescope lsp_document_symbols<CR>",
+        { noremap = true, silent = true }
+      )
 
-						-- pseudo code / specification for writing custom displays, like the one
-						-- for "codeactions"
-						-- specific_opts = {
-						--   [kind] = {
-						--     make_indexed = function(items) -> indexed_items, width,
-						--     make_displayer = function(widths) -> displayer
-						--     make_display = function(displayer) -> function(e)
-						--     make_ordinal = function(e) -> string
-						--   },
-						--   -- for example to disable the custom builtin "codeactions" display
-						--      do the following
-						--   codeactions = false,
-						-- }
-					},
-				},
-			})
-			-- To get ui-select loaded and working with telescope, you need to call
-			-- load_extension, somewhere after setup function:
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      -- Highlight groups can go here or elsewhere, but after termguicolors = true
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({
+              -- even more opts
+              defaults = {
+                border = true,
+              },
+            }),
+
+            -- pseudo code / specification for writing custom displays, like the one
+            -- for "codeactions"
+            -- specific_opts = {
+            --   [kind] = {
+            --     make_indexed = function(items) -> indexed_items, width,
+            --     make_displayer = function(widths) -> displayer
+            --     make_display = function(displayer) -> function(e)
+            --     make_ordinal = function(e) -> string
+            --   },
+            --   -- for example to disable the custom builtin "codeactions" display
+            --      do the following
+            --   codeactions = false,
+            -- }
+          },
+        },
+      })
+      -- To get ui-select loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
