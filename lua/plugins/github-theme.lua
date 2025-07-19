1faa6de78023e79cf3aec392244f8348abd05e03
mmallya <m.mallya@trianel.com>
2025-07-19 19:38:00 +0200

    .

--- lua/plugins/github-theme.lua
+++ lua/plugins/github-theme.lua
@@ -9,11 +9,11 @@ return {
       terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
       dim_inactive = true, -- Non focused panes set to alternative background
     })
-    vim.cmd("colorscheme github_light")
-
-    vim.api.nvim_set_hl(0, "Comment", {
-    	fg = "#988683", -- your desired color (e.g., a bright orange)
-    	italic = false,
-    })
+    -- vim.cmd("colorscheme github_light")
+    --
+    -- vim.api.nvim_set_hl(0, "Comment", {
+    -- 	fg = "#988683", -- your desired color (e.g., a bright orange)
+    -- 	italic = false,
+    -- })
   end,
 }
