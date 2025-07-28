29ba0b72dd67b4ae43b07c09183365581011398f
mmallya <m.mallya@trianel.com>
2025-07-28 22:44:29 +0200

    Theme changes

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("onedark").setup({
-        style = "darker",
+        style = "warmer",
         code_style = {
           comments = "bold",
           keywords = "bold",
