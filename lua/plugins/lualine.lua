0b0aca89e3f0f4fb003e7dab6e9153438061d414
Mohit Radhesh Mallya <mmallya775@gmail.com>
2026-01-31 11:53:42 +0100

    added gitsigns and some theme changes and trouble plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     --Lua-line setup
     require("lualine").setup({
-      options = { theme = "material" },
+      options = { theme = "catppuccin" },
       sections = {
         lualine_c = { { "filename", path = 1 } },
       },
