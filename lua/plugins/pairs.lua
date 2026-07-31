507c05b991c6f6ee7fdb463f2af9198e3c0ca852
Mohit Radhesh Mallya <m.mallya@trianel.com>
2026-07-31 11:43:15 +0200

    fix unbalanced parens while backspacing in clojure

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -2,7 +2,21 @@ return {
   {
     "nvim-mini/mini.pairs",
     opts = {
-      skip_unbalanced = false,
+      skip_unbalanced = true,
     },
+    config = function(_, opts)
+      LazyVim.mini.pairs(opts)
+
+      local function map_pair_backspace(key)
+        vim.keymap.set("i", key, "v:lua.MiniPairs.bs()", {
+          expr = true,
+          replace_keycodes = false,
+          desc = "Pair-aware backspace",
+        })
+      end
+
+      map_pair_backspace("<BS>")
+      map_pair_backspace("<C-h>")
+    end,
   },
 }
