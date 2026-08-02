e7a88c98b23a3ec061300bf2c916a7602448b85f
Mohit Radhesh Mallya <mmallya775@gmail.com>
2026-08-02 15:36:11 +0200

    Plugin to add package aware java classes and interfaces

---
+++ lua/plugins/java-creator.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "alessio-vivaldelli/java-creator-nvim",
+    ft = "java",
+    opts = {
+      keymaps = {
+        java_new = "<leader>jn",
+      },
+      options = {
+        java_version = 25,
+        auto_open = true,
+      },
+    },
+  },
+}
