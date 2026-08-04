4befc8ec328bf81b92d91100ecebac1396aa8c59
Mohit Radhesh Mallya <m.mallya@trianel.com>
2026-08-04 09:06:46 +0200

    Fix weird folding in cljc files

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,10 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 
 vim.opt.fixendofline = false
+
+-- Always open files with all folds expanded
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
+-- Do not restore fold state from LazyVim sessions
+vim.opt.sessionoptions:remove("folds")
